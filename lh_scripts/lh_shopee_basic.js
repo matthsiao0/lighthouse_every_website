1ec56af22ecd23a90d853fc3ab984f2bfa340ac9
matthsiao0 <matt.hsiao@swag.live>
2023-04-11 00:06:03 +0800

    modify shopee lh cases

--- lh_scripts/lh_shopee_basic.js
+++ lh_scripts/lh_shopee_basic.js
@@ -1,17 +1,22 @@
+// This is edited from: https://github.com/GoogleChrome/lighthouse/blob/main/docs/readme.md
+// File name and type: shopee_lh.js
+// note: lh for lighthouse
 import fs from 'fs';
 import lighthouse from 'lighthouse';
 import chromeLauncher from 'chrome-launcher';
 
+// Chrome Launcher Setup
 const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+const options = {logLevel: 'info', output: 'html', port: chrome.port};
 const runnerResult = await lighthouse('https://shopee.tw/', options);
 
-// `.report` is the HTML report as a string
+// Generate report to given file
 const reportHtml = runnerResult.report;
-fs.writeFileSync('../.reports/shopee_lighthouse_report.html', reportHtml);
+fs.writeFileSync('./.reports/lh_shopee_basic_report.html', reportHtml);
 
-// `.lhr` is the Lighthouse Result as a JS object
+// Print Message and Score in Terminal
 console.log('Report is done for', runnerResult.lhr.finalDisplayedUrl);
 console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
+// Teardown
 await chrome.kill();
--- lh_scripts/lh_shopee_search_product_and_open_detail.js
+++ lh_scripts/lh_shopee_search_product_and_open_detail.js
@@ -7,32 +7,43 @@ const browser = await puppeteer.launch();
 const page = await browser.newPage();
 const flow = await startFlow(page);
 
+// Delete Cookies and Disable Cache
+await page.deleteCookie()
+await page.setCacheEnabled(false)
+
 // 1. Navigate to Shopee
 await flow.navigate('https://shopee.tw/');
 
-// 2. Start Timespan audit for Closing advertisement overlay & Searching for Product
+new Promise(r => setTimeout(r, 1000));
+// 2. Close ad
+await page.mouse.click(180, 0)
+
+// 3. Start Timespan audit for Searching Product
 await flow.startTimespan()
-
-  // 2.1. Close ad
-const close_ad_button = await page.$('')
-await close_ad_button.click()
-
-  // 2.2. Search for product
+  
+  // 3.1. Search for product
 const search_field = await page.$('input[class="stardust-input__element search-bar__input search-bar__input--orange"]')
 
-    // 2.2.1. Delay 100 is for simulating user type in
-await search_field.type('Lovense', {delay:100})
+    // 3.1.1. Delay 100 is for simulating user type in
+await search_field.type('iphone15', {delay:100})
 
-  // 2.3. Press Enter and wait for result page opened
+  // 3.2. Press Enter and wait for result page opened
 await Promise.all([
     search_field.press('Enter'),
     page.waitForNavigation(),
   ])
 await flow.endTimespan()
 
-// 3. analyze current state
+// 4. Start Navigation audit
+await flow.startTimespan()
+const first_product = await page.$('img')
+await first_product.click()
+await flow.endTimespan()
+
+
+// 5. Analyze Product detail page state
 await flow.snapshot()
 
 // Teardown: Close browser and generate report
 await browser.close();
-writeFileSync('../.report/lh_shopee_search_product_and_open_detail.html', await flow.generateReport());
+writeFileSync('./.reports/lh_shopee_search_product_and_open_detail.html', await flow.generateReport());
